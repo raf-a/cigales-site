c99a8d5e3fc1ccb6553ad29a045f183786942b5c
Rafael Arazola <rarazola@keley-live.com>
2021-09-10 10:18:44 +0200

    Social Media

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,9 @@ import { Document } from "@prismicio/client/types/documents";
 import React, { VFC } from "react";
 import Menu from "./Menu";
 import PrismicRichText from "./PrismicRichText";
+import Link from "next/link";
+import { getLinkUrl } from "utils/prismic";
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
 
 const Footer: VFC<{ homepageDoc: Document }> = ({ homepageDoc }) => (
   <footer>
@@ -16,6 +19,17 @@ const Footer: VFC<{ homepageDoc: Document }> = ({ homepageDoc }) => (
     <div className="contact">
       <h2>Contact</h2>
       <PrismicRichText render={homepageDoc.data.contact_info} />
+    </div>
+    <div className="social-media">
+      {homepageDoc.data.social_media.map((item: any, key: number) => (
+        <Link key={key} href={getLinkUrl(item.link)}>
+          <a title={item.name}>
+            {item.name === "Facebook" && <FaFacebookF />}
+            {item.name === "Twitter" && <FaTwitter />}
+            {item.name === "LinkedIn" && <FaLinkedinIn />}
+          </a>
+        </Link>
+      ))}
     </div>
     <style jsx>{`
       footer {
@@ -38,9 +52,22 @@ const Footer: VFC<{ homepageDoc: Document }> = ({ homepageDoc }) => (
       .contact {
         grid-column: 1 / 3;
       }
+      .social-media a {
+        display: inline-block;
+        padding: 0.5rem;
+        font-size: 1.5rem;
+        width: 1.5rem;
+        height: 1.5rem;
+        border-radius: 1.5rem;
+        background-color: var(--color-text2);
+        color: var(--color-bg2);
+      }
+      .social-media a:not(:first-child) {
+        margin-left: 0.5rem;
+      }
       @media (min-width: 1024px) {
         footer {
-          grid-template-columns: 1fr 1fr 2fr;
+          grid-template-columns: 1fr 1fr 2fr auto;
           column-gap: 40px;
         }
         .contact {

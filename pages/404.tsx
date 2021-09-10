7853ffa63d00a99411a1ba58312c7597bae68d28
Rafael Arazola <rarazola@keley-live.com>
2021-09-10 18:52:06 +0200

    404 error page

---
+++ pages/404.tsx
@@ -0,0 +1,28 @@
+import { Document } from "@prismicio/client/types/documents";
+import Layout from "components/Layout";
+import MainContent from "components/MainContent";
+import PageHeader from "components/PageHeader";
+import { GetStaticProps, NextPage } from "next";
+import { getHomepage } from "utils/prismic";
+
+type PageNotFoundProps = {
+  homepageDoc: Document;
+};
+
+const PageNotFound: NextPage<PageNotFoundProps> = ({ homepageDoc }) => (
+  <Layout homepageDoc={homepageDoc}>
+    <MainContent title="404" body={<p>Désolé, cette page n’existe pas.</p>} />
+  </Layout>
+);
+
+export default PageNotFound;
+
+export const getStaticProps: GetStaticProps<PageNotFoundProps> = async ({
+  previewData,
+}) => {
+  const homepageDoc = await getHomepage(previewData);
+
+  return {
+    props: { homepageDoc },
+  };
+};

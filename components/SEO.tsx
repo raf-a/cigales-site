9a563c88d69dc669be9f9f7e4d4eac8ec35008cb
Rafael Arazola <rarazola@keley-live.com>
2021-09-14 18:31:12 +0200

    Open Graph

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -1,23 +1,34 @@
 import React, { VFC } from "react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 type SEOProps = {
   title: string;
   description?: string;
   image?: string;
-  siteName?: string;
+  siteName: string;
 };
 
-const SEO: VFC<SEOProps> = ({ title, description, image, siteName }) => (
-  <Head>
-    {title && <title>{title}</title>}
-    {description && <meta name="description" content={description} />}
+const SEO: VFC<SEOProps> = ({ title, description, image, siteName }) => {
+  const router = useRouter();
 
-    {title && <meta property="og:title" content={title} />}
-    {description && <meta property="og:description" content={description} />}
-    {image && <meta property="og:image" content={image} />}
-    {siteName && <meta property="og:site_name" content={siteName} />}
-  </Head>
-);
+  return (
+    <Head>
+      <title>{title}</title>
+      {description && <meta name="description" content={description} />}
+
+      <meta
+        property="og:url"
+        content={process.env.NEXT_PUBLIC_SITE_URL.concat(router.asPath)}
+      />
+      <meta property="og:title" content={title} />
+      {description && <meta property="og:description" content={description} />}
+      {image && <meta property="og:image" content={image} />}
+
+      <meta property="og:site_name" content={siteName} />
+      <meta property="og:locale" content="fr_FR" />
+    </Head>
+  );
+};
 
 export default SEO;
